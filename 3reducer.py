6fae85ecdd96ce3eff1cab5f5bf6fea05eb6a0ce
DeepakJayanthi <46684265+DeepakJayanthi@users.noreply.github.com>
2020-02-12 14:31:07 -0600

    Calculated count of records for each Payment type.

--- 3reducer.py
+++ 3reducer.py
@@ -4,24 +4,38 @@ r = open("03.txt", "w")
 thisKey = ""
 thisValue = 0.0
 
+
+counter = 0
+old_payment_value = None
+
+
 for line in s:
   data = line.strip().split('\t')
-  store, amount = data
+  paymentType, amount = data
 
-  if store != thisKey:
-    if thisKey:
-      # output the last key value pair result
-      r.write(thisKey + '\t' + str(thisValue)+'\n')
+  if paymentType != thisKey:
+    # if thisKey:
+    #   # output the last key value pair result
+    #   #r.write(thisKey + '\t' + str(thisValue)+'\n')
 
     # start over when changing keys
-    thisKey = store 
+    thisKey = paymentType 
     thisValue = 0.0
   
   # apply the aggregation function
-  thisValue += float(amount)
+  #thisValue += float(amount)
+  if old_payment_value is None:
+    old_payment_value = paymentType
+
+  if old_payment_value == paymentType:
+    counter = counter + 1
+  else:
+    r.write(old_payment_value + '\t' + str(counter)+'\n' )
+    counter = 1
+    old_payment_value = paymentType
 
 # output the final entry when done
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(old_payment_value + '\t' + str(counter)+'\n' )
 
 s.close()
 r.close()
